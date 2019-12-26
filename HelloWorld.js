0b646fcb94e67aff5b1c7423c1a40989c6522eda
csec <csec-hebnu@163.com>
2019-12-26 20:25:17 +0800

    '响应体验'

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,13 @@
 var http = require('http')
 var server = http.createServer()
-server.on('request',()=>{
-  console.log('接受请求成功')
+server.on('request',(req,res)=>{
+  if(req.url==='/login')
+    {res.end('login page')}
+  else if(req.url==='/index')
+    {res.end('index page')}
+  else{
+    res.end('404 not found')
+  }
 })
 server.listen(3000,()=>{
   console.log('server created')
